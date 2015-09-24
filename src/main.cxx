b5048214c72a42a5bd3396bdc9ba6e652a7ee4d5
Benoit Gschwind <gschwind@gnu-log.net>
2015-09-24 22:29:22 +0200

    add xdg-shell-protocol

--- src/main.cxx
+++ src/main.cxx
@@ -8,14 +8,16 @@
  */
 
 #include <cstdio>
+#include <cstdlib>
 
 #include <poll.h>
-
-
 #include <wayland-server.h>
+
 #include <compositor.h>
 #include <compositor-x11.h>
 #include <compositor-drm.h>
+
+#include "xdg-shell-server-protocol.h"
 
 
 struct callbacks {
@@ -43,16 +45,36 @@ struct callbacks {
 
 };
 
+struct output_callback {
+	wl_listener frame;
+	wl_listener destroy;
+};
+
+wl_global_bind_func_t test;
+
 int main(int argc, char ** argv) {
 	struct callbacks cbk;
+	output_callback ocbk;
 
     weston_log_set_handler(vprintf, vprintf);
 
 	/* first create the wayland serveur */
-	wl_display * dpy = wl_display_create();
+	auto dpy = wl_display_create();
+
+	auto sock_name = wl_display_add_socket_auto(dpy);
+	weston_log("socket name = %s\n", sock_name);
+	setenv("WAYLAND_DISPLAY", sock_name, 1);
+
+	if (wl_global_create(dpy, &wl_shell_interface, 1,
+				  nullptr, [](wl_client * client, void * data, uint32_t version, uint32_t id){ weston_log("bind wl_shell_interface\n");  }) == NULL)
+		return -1;
+
+	if (wl_global_create(dpy, &xdg_shell_interface, 1,
+				  nullptr, [](wl_client * client, void * data, uint32_t version, uint32_t id){ weston_log("bind xdg_shell_interface\n");  }) == NULL)
+		return -1;
 
 	/* initialise the compositor */
-	weston_compositor * cmp = weston_compositor_create(dpy, NULL);
+	auto cmp = weston_compositor_create(dpy, NULL);
 
 	/* setup the keyboard layout (MANDATORY) */
 	xkb_rule_names names = {
@@ -69,8 +91,17 @@ int main(int argc, char ** argv) {
 	conf.use_pixman = 0;
 
 	/* initialize the x11-backend */
-	weston_compositor_init_backend(cmp, "x11-backend.so", (struct weston_backend_config*)&conf);
+	weston_compositor_init_backend(cmp, "x11-backend.so", &conf.base);
 
+	weston_x11_backend_output_config output_config = { { WL_OUTPUT_TRANSFORM_NORMAL, 800, 600, 1 }, 0 };
+
+	weston_output * output = cmp->backend->create_output(cmp, NULL, &output_config.base);
+
+    wl_signal_add(&output->frame_signal, &ocbk.frame);
+    ocbk.frame.notify = [](wl_listener *l, void *data) { weston_log("output.frame\n"); };
+
+    wl_signal_add(&output->destroy_signal, &ocbk.destroy);
+    ocbk.destroy.notify = [](wl_listener *l, void *data) { weston_log("output.destroy\n"); };
 
     wl_signal_add(&cmp->destroy_signal, &cbk.destroy);
     cbk.destroy.notify = [](wl_listener *l, void *data) { weston_log("destroy\n"); };
@@ -87,20 +118,11 @@ int main(int argc, char ** argv) {
     wl_signal_add(&cmp->seat_created_signal, &cbk.seat_created);
     cbk.seat_created.notify = [](wl_listener *l, void *data) { weston_log("seat_created\n"); };
 
-    wl_event_loop * loop = wl_display_get_event_loop(dpy);
-    int fd = wl_event_loop_get_fd(loop);
-
-	pollfd poll_list;
-	poll_list.fd = fd;
-	poll_list.events = POLLIN|POLLOUT|POLLERR;
+    wl_signal_add(&cmp->create_surface_signal, &cbk.create_surface);
+    cbk.create_surface.notify = [](wl_listener *l, void *data) { weston_log("create_surface\n"); };
 
 
-    bool running = true;
-    while(running) {
-    	poll(&poll_list, 1, 0);
-        wl_event_loop_dispatch(loop, 0);
-        wl_display_flush_clients(dpy);
-    }
+    wl_display_run(dpy);
 
 }
 
